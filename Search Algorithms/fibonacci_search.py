5936101d1b01b71b1df5c7319ddfddccba9101b9
Quinston Pimenta <quinstonpimenta@outlook.com>
2018-04-22 10:42:53 +0530

    tim_sort.py

--- Search Algorithms/fibonacci_search.py
+++ Search Algorithms/fibonacci_search.py
@@ -1,43 +1,47 @@
 # return the fibonacci number at the index of n
 def FibonacciGenerator(n):
-    if n < 1: return 0
-    elif n == 1: return 1
-    else: return FibonacciGenerator(n - 1) + FibonacciGenerator(n - 2)
+    if n < 1:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return FibonacciGenerator(n - 1) + FibonacciGenerator(n - 2)
 
 # return the index at which x exists inside arr
 # return -1 otherwise
+
 def FibonacciSearch(arr, x):
 
-    # find the smallest Fibonacci number greater than or equal 
+    # find the smallest Fibonacci number greater than or equal
     # to the length of arr
     m = 0
-    while FibonacciGenerator(m) < len(arr):
-        m = m + 1
+    while FibonacciGenerator(m) < len(arr): # 
+        m = m + 1 
 
     # m now contains the index of the the smallest Fibonacci
-    # number greater than or equal to the length of tha array
+    # number greater than or equal to the length of the array
     # for example
-    # if the length of arr is 10, FibonacciGenerator(m) should be 13
-    
-    # this is the length of that array from the 
+    # if the length of arr is 11, FibonacciGenerator(m) should be 13
+
+    # this is the length of that array from the
     # start that has been eliminated
     offset = -1
- 
+
     # make sure you fibonacci index is valid
     while (FibonacciGenerator(m) > 1):
-         
+
         # 1. you are generating a number greater the length
         # of the array the first time you run it
         # 2. you're adding it with the offset
         # 3. you're comparing it with the length of the array
         # 4. after running for the first time, you will always
         # get returned the first parameter of min()
-        i = min(offset + FibonacciGenerator(m - 2), len(arr) - 1)
- 
-        # if the value you are searching for is greater than the 
-        # current arr[i], then it clearly means that you need to 
+        i = min( offset + FibonacciGenerator(m - 2) , len(arr) - 1)
+
+        # if the value you are searching for is greater than the
+        # current arr[i], then it clearly means that you need to
         # shift your focus to the elements that are beyond arr[i]
-        # move the three fibonacci variables one fibonacci down. 
+        # move the three fibonacci variables one fibonacci down.
         # Reset offset to index. Together these indicate elimination
         # of approximately front one-third of the remaining array.
         if (x > arr[i]):
@@ -45,32 +49,32 @@ def FibonacciSearch(arr, x):
             m = m - 1
             offset = i
 
-        # if the value you are searching for is less than the 
-        # current arr[i], then it clearly means that you need to 
+        # if the value you are searching for is less than the
+        # current arr[i], then it clearly means that you need to
         # shift your focus to the elements that are before arr[i]
-        # move the three fibonacci variables two fibonacci down, 
-        # indicating elimination of approximately rear two-third 
+        # move the three fibonacci variables two fibonacci down,
+        # indicating elimination of approximately rear two-third
         # of the remaining array
         elif (x < arr[i]):
 
             m = m - 2
- 
+
         # naturally if both the values are the same, then you
         # have reached your answer and you can then return the
         # the index of the array
-        else :
+        else:
 
             return i
- 
+
     # this will run if there is one last element left
     if(FibonacciGenerator(m - 1) and arr[offset + 1] == x):
         return offset + 1
- 
+
     # return -1 if the element doesn't exist in the array
     return -1
 
 
 # the search array
-arr = [ 10, 22, 30, 44, 56, 58, 60, 70, 100, 110, 130 ]
-x = 10
+arr = [10, 22, 30, 44, 56, 58, 60, 70, 100, 110, 130] # 13
+x = 44
 print(FibonacciSearch(arr, x))
---
+++ Sorting Algorithms/tim_sort.py
@@ -0,0 +1,67 @@
+# SORTING AN ARRAY TO THE ASCENDING ORDER
+
+
+def InsertionSort(array):
+
+    for x in range (1, len(array)):
+        for i in range(x, 0, -1):
+            if array[i] < array[i - 1]:
+                t = array[i]
+                array[i] = array[i - 1]
+                array[i - 1] = t
+            else:
+                break
+            i = i - 1
+    return array
+
+def Merge(aArr, bArr):
+    
+    a = 0
+    b = 0
+    cArr = []
+
+    while a < len(aArr) and b < len(bArr):
+        if aArr[a] < bArr[b]:
+            cArr.append(aArr[a])
+            a = a + 1
+        elif aArr[a] > bArr[b]:
+            cArr.append(bArr[b])
+            b = b + 1
+        else:
+            cArr.append(aArr[a])
+            cArr.append(bArr[b])
+            a = a + 1
+            b = b + 1
+
+    while a < len(aArr):
+        cArr.append(aArr[a])
+        a = a + 1
+
+    while b < len(bArr):
+        cArr.append(bArr[b])
+        b = b + 1
+
+    return cArr
+
+def TimSort():
+
+    # run insertion sort on the RUNs
+    for x in range(0, len(arr), RUN):
+        arr[x : x + RUN] = InsertionSort(arr[x : x + RUN])
+
+    # merge the runs (RUN => RUN * 2)
+    RUNinc = RUN
+    while RUNinc < len(arr):
+
+        for x in range(0, len(arr), 2 * RUNinc):
+            arr[x : x + 2 * RUNinc] = Merge(arr[x : x + RUNinc], arr[x + RUNinc: x + 2 * RUNinc])
+
+        RUNinc = RUNinc * 2
+
+import random
+arr = []
+RUN = 32
+for x in range(0, 50):
+    arr.append(random.randint(0, 100))
+TimSort()
+print(arr)
