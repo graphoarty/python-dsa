86de72e4d8eec3c139099d7d08864b149b81374d
Quinston Pimenta <quinstonpimenta@outlook.com>
2018-05-23 10:07:35 +0530

    corrections

--- Sorting Algorithms/cocktail_sort.py
+++ Sorting Algorithms/cocktail_sort.py
@@ -1,51 +1,32 @@
-
 def CocktailSort(array):
 
-    n = len(array)
-    
-    swapped = True
+    isSwapped = True
     start = 0
-    end = n - 1
-    while (swapped == True):
+    end = len(array) - 1
 
-        # reset the swapped flag on entering the loop,
-        # because it might be true from a previous
-        # iteration.
-        swapped = False
+    while (isSwapped == True):
 
-        # loop from left to right same as the bubble
-        # sort
+        isSwapped = False
+
         for i in range(start, end):
             if (array[i] > array[i + 1]):
                 array[i], array[i + 1] = array[i + 1], array[i]
-                swapped = True
+                isSwapped = True
 
-        # if nothing moved, then array is sorted.
-        if (swapped == False):
+        if (isSwapped == False):
             break
 
-        # otherwise, reset the swapped flag so that it
-        # can be used in the next stage
-        swapped = False
+        isSwapped = False
 
-        # move the end point back by one, because
-        # item at the end is in its rightful spot
         end = end - 1
 
-        # from right to left, doing the same
-        # comparison as in the previous stage
         for i in range(end - 1, start - 1, -1):
             if (array[i] > array[i + 1]):
                 array[i], array[i + 1] = array[i + 1], array[i]
-                swapped = True
+                isSwapped = True
 
-        # increase the starting point, because
-        # the last stage would have moved the next
-        # smallest number to its rightful spot.
         start = start + 1
 
+    return array
 
-# Driver code to test above
-array = [5, 1, 4, 2, 8, 0, 2]
-CocktailSort(array)
-print(array)
+print(CocktailSort([31, 81, 84, 23, 63, 57, 16, 25, 63, 40]))
