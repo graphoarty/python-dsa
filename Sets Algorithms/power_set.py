251abfc5b2b01c57bdd2c02d62de29c7929440a1
Quinston Pimenta <quinstonpimenta@outlook.com>
2018-06-11 10:56:28 +0530

    power set

---
+++ Sets Algorithms/power_set.py
@@ -0,0 +1,20 @@
+def PowerSet(originalSet):
+
+    subSets = []
+
+    numberOfCombinations = 2 ** len(originalSet)
+
+    for combinationIndex in range(0, numberOfCombinations):
+
+        subSet = []
+
+        for setElementIndex in range(0, len(originalSet)):
+
+            if combinationIndex & 1 << setElementIndex:
+                subSet.append(originalSet[setElementIndex])
+
+        subSets.append(subSet)       
+
+    return subSets
+
+print(PowerSet(['1', '2', '3']))
