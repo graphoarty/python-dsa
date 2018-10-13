7cbc36216c1ab410a6f686417776bdc97793d381
Quinston Pimenta <quinstonpimenta@outlook.com>
2018-10-13 14:11:29 +0530

    bellman-ford

---
+++ Graph Algorithms/bellman-ford.py
@@ -0,0 +1,50 @@
+'''
+
+           0  1  2  3  4
+
+    0    [ 0, 6, 0, 4, 0 ],
+    1    [ 0, 0, 3, 2, 0 ],
+    2    [ 0, 0, 0, 0, 4 ],
+    3    [ 0, 1, 9, 0, 3 ],
+    4    [ 7, 0, 5, 0, 0 ]
+
+'''
+
+from array import *
+
+MAX = 999
+
+adjacencyArray = [
+
+    [ 0, 6, MAX, 4, MAX ],
+    [ MAX, MAX, 3, 2, MAX ],
+    [ MAX, MAX, MAX, MAX, 4 ],
+    [ MAX, 1, 9, MAX, 3 ],
+    [ 7, MAX, 5, MAX, MAX ]
+
+]
+
+shortest = [0, MAX, MAX, MAX, MAX, MAX]
+pred = [None, None, None, None, None]
+
+# Relax Functions
+for u in range(0, 5):
+    for v in range(0, 5):
+
+        if shortest[u] + adjacencyArray[u][v] < shortest[v]:
+            shortest[v] = shortest[u] + adjacencyArray[u][v]
+            pred[v] = u
+
+for x in range(0, 5):
+
+    v = x
+
+    print('Path for vertex ' + str(v), end=': ')
+
+    path = ''
+
+    while pred[v] != None:
+        v = pred[v]
+        path += str(v) + ' '
+        
+    print(path[::-1] + ' ' + str(x))
---
+++ Sorting Algorithms/bs.py
@@ -0,0 +1,41 @@
+
+def BitonicSort(direction, arr):
+
+    if len(arr) <= 1:
+
+        return arr
+
+    else: 
+
+        first = BitonicSort(True, arr[:len(arr) // 2])
+        second = BitonicSort(False, arr[len(arr) // 2:])
+
+        return BitonicMerge(direction, first + second)
+
+def BitonicMerge(direction, arr): 
+
+    if len(arr) == 1:
+
+        return arr
+
+    else:
+
+        BitonicCompare(direction, arr)
+
+        first = BitonicMerge(direction, arr[:len(arr) // 2])
+        second = BitonicMerge(direction, arr[len(arr) // 2:])
+        
+        return first + second
+
+def BitonicCompare(direction, arr):
+
+    dist = len(arr) // 2
+    
+    for i in range(dist):  
+
+        if (arr[i] > arr[i + dist]) == direction:
+            arr[i], arr[i + dist] = arr[i + dist], arr[i]
+
+array = [ 3, 7, 4, 8, 6, 2, 1, 5 ]
+array = BitonicSort(True, array)
+print(array)
