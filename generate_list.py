34af4afaee34ec76ea3968ac8be47580bfa596bf
Quinston Pimenta <quinstonpimenta@outlook.com>
2021-05-11 14:03:22 +0530

    New ReadMe Format

--- generate_list.py
+++ generate_list.py
@@ -1,13 +1,12 @@
 import os
 
-def TraverseDirectory(directory, space):
+def TraverseDirectory(directory, space, hashes):
     for item in os.scandir(directory):
         if not item.is_file():
             if not (item.name.__contains__('.git') or item.name.__contains__('__pycache__')):
-                print(space + item.name + '<br />')
-                TraverseDirectory(os.path.join(directory, item.name), space + '&nbsp;')
+                print(hashes + ' ' + item.name + '<br />')
+                TraverseDirectory(os.path.join(directory, item.name), space + '&nbsp;&nbsp;&nbsp;&nbsp;', hashes + '#')
         else:
             print(space + item.name + '<br />')
 
-TraverseDirectory('.', '')
-        
+TraverseDirectory('.', '', '##') 
